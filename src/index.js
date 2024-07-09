fc15f8e33e806c07a5858e837788335b276d5354
Rakyarvik <112162348+Rakyarvik@users.noreply.github.com>
2024-07-09 18:38:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.rakyarvik.top": dockerHub,
+  "quay.rakyarvik.top": "https://quay.io",
+  "gcr.rakyarvik.top": "https://gcr.io",
+  "k8s-gcr.rakyarvik.top": "https://k8s.gcr.io",
+  "k8s.rakyarvik.top": "https://registry.k8s.io",
+  "ghcr.rakyarvik.top": "https://ghcr.io",
+  "cloudsmith.rakyarvik.top": "https://docker.cloudsmith.io",
+  "ecr.rakyarvik.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.rakyarvik.top": dockerHub,
 };
 
 function routeByHosts(host) {
